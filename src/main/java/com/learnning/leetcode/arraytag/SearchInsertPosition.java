a3bfc0a0357491630aabbdd0272b1ff58f449af9
gaojun416 <540957506@qq.com>
2017-10-31 10:10:32 +0800

    Search Insert Position 使用二分法

--- src/main/java/com/learnning/leetcode/arraytag/SearchInsertPosition.java
+++ src/main/java/com/learnning/leetcode/arraytag/SearchInsertPosition.java
@@ -2,15 +2,19 @@ package com.learnning.leetcode.arraytag;
 
 public class SearchInsertPosition {
 	public int searchInsert(int[] nums, int target) {
-        int position = 0;
         if(nums.length == 0)return 0;
-        for(int i = 0;  i < nums.length ; i++){
-            if(nums[i] >= target){
-                break;
-            }
-            position++;
-        }
+        return binarySearch(nums , 0 , nums.length-1 , target);
         
-        return position;
+    }
+    
+    public int binarySearch(int[] nums , int left , int right , int target){
+        if(right>=left){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < target) return binarySearch(nums , mid + 1, right ,target);
+            if(nums[mid] > target) return binarySearch(nums , left , mid - 1, target);
+            
+        }
+        return left;
     }
 }
